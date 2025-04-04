f45a0e3d747c10d95ec9a3f1d701166265190e84
klawicki904 <klawicki@mat.umk.pl>
2025-04-04 18:31:25 +0200

    Update Map.cpp
    Added comments, separated BFS and Ford-Fulkerson algorithms into separate functions.

--- source CPP/Map.cpp
+++ source CPP/Map.cpp
@@ -8,13 +8,12 @@
 Do zrobienia dla ca³ego projektu:
 1. Generator grafów (plik z po³¹czeniami) do testowania algorytmu.
 2. GUI
+3. Dokumentacja
 
 Do zrobienia dla pierwszej czêœci:
 1a. Przerobiæ ten plik tak, ¿eby wczytywa³ z pliku node'y zamiast po³¹czeñ
 1b. Przerobiæ ten plik tak, ¿eby generowa³ plik z po³¹czeniami na podstawie wczytanych node'ów
 2. Browary przemna¿aj¹ce przechodz¹c¹ wartoœæ
-
-
 */
 
 //#include "Node.h"
@@ -32,7 +31,9 @@ Do zrobienia dla pierwszej czêœci:
 
 using namespace std;
 
-double beerConvertsionRate = 0.5;
+double beerConvertsionRate = 0.5; // jeszcze nie u¿yte
+// mo¿emy zapisaæ ka¿dy typ w oddzielnej klasie, ale bêd¹ przechowywaæ co najwy¿ej swoje po³o¿enie i jedn¹ zmienn¹
+// (jeszcze nie u¿yte) rodzaj wêz³a (¿adne (Ÿród³o lub ujœcie), pole, browarnia, karczma)
 enum NodeType
 {
     none,
@@ -40,20 +41,140 @@ enum NodeType
     brewery,
     pub
 };
-
+// (jeszcze nie u¿yte) struktura wektor do przechowania po³o¿enia wierzcho³ka
 struct Vector2
 {
-    int x;
-    int y;
+    int x = 0;
+    int y = 0;
 };
 
-struct edgeData
+// struktura trzymaj¹ca informacje o drodze (strza³ce) miêdzy wêz³ami
+struct Road
 {
-    NodeType nodeType = none;
-    Vector2 position;
-    int to;
-    double remainingFlow;
+    NodeType nodeType = none; // jeszcze nie u¿ywane
+    Vector2 position; // jeszcze nie u¿ywane
+    int to = -1; // indeks wierzcho³ka, do którego droga prowadzi
+    double remainingFlow = 0; // pozosta³y przep³yw
 };
+
+void bfsFlow(vector<vector<Road>> &aList, vector<int> &parents, vector<double> &flows, int start = 0)
+{
+    vector<bool> visited(aList.size(), false); // odwiedzone 
+    queue<int> Q; // kolejka
+    //parents[start] = -2;
+
+    visited[start] = 1;
+    Q.push(start); // na pocz¹tek dodaje wêze³ startowy do kolejki
+    int v = 0;
+    while (!Q.empty())
+    {
+        int u = Q.front(); // zapisuje element na pocz¹tku kolejki
+        Q.pop(); // usuwa element z pocz¹tku kolejki
+        for (int i = 0; i < aList[u].size(); i++) // dla ka¿dej drogi z u
+        {
+            v = aList[u][i].to;
+            // dodaje wêze³ v do kolejki jeœli nieodwiedzony i ma dostêpny przep³yw
+            if (visited[v] == false && aList[u][i].remainingFlow > 0)
+            {   // po dodaniu:
+                visited[v] = true; // wêze³ ma status odwiedzony
+                parents[v] = u; // zapisuje u jako poprzednik v
+                flows[v] = min(flows[u], aList[u][i].remainingFlow); // aktualizuje maksymalny przep³yw dla wêz³a
+                Q.push(v); // dodaje wêze³ v do kolejki
+            }
+        }
+    }
+}
+
+double fordFulkerson(vector<vector<Road>>& aList)
+{
+    double totalFlow = 0;
+    int vertices = aList.size();
+    while (true) // wykonuje, a¿ nie znajdzie œcie¿ki powiêkszaj¹cej
+    {
+        /// wyswietla stan listy sasiedztwa
+        cout << "\aLista sasiedztwa: z: ( do, pozostaly ):\n";
+        for (int i = 0; i < vertices - 1; i++)
+        {
+            cout << i << ": \t";
+            for (unsigned int j = 0; j < aList[i].size(); j++)
+            {
+                cout << "( " << aList[i][j].to << ", " << aList[i][j].remainingFlow << " )\t";
+            }
+            cout << endl;
+        }
+        // BFS
+        int start = 0; // indeks wêz³a startowego
+        vector<int> parents(vertices, -1); // poprzedniki, na pocz¹tku wszystkie -1
+        vector<double> flows(vertices, INT_MAX); // maksymalny przep³yw do ka¿dego wêz³a
+
+        bfsFlow(aList, parents, flows); // BFS
+
+        // wypisuje tablicê poprzedników
+        cout << "\nparents:\n";
+        for (int i = 0; i < parents.size(); i++)
+        {
+            cout << i << ": " << parents[i] << "\n";
+        }
+        // wypisuje tablicê przep³ywów
+        cout << "\nflows:\n";
+        for (int i = 0; i < flows.size(); i++)
+        {
+            cout << i << ": " << flows[i] << "\n";
+        }
+
+        int finish = vertices - 1;
+        /// nie znaleziono sciezki do ujœcia, koniec algorytmu FF
+        if (parents[finish] == -1) break;
+
+        // zapisz œcie¿kê powiêkszaj¹c¹ na podstawie tablicy poprzedników
+        vector<int> shortestPath;
+        cout << "\nShortest path:\n";
+        shortestPath.push_back(finish);
+        do
+        {
+            finish = parents[finish];
+            shortestPath.push_back(finish);
+        } while (finish != start);
+
+        finish = vertices - 1;
+        reverse(shortestPath.begin(), shortestPath.end());
+
+        // wymieñ wierzcho³ki tworz¹ce œcie¿kê powiêkszaj¹c¹
+        for (int i = 0; i < shortestPath.size(); i++)
+        {
+            cout << shortestPath[i] << ", ";
+        }
+        cout << endl;
+
+        cout << "Max Flow In This Path: " << flows[finish] << endl;
+        totalFlow += flows[finish];
+
+        // zaktualizuj przep³yw w liœcie s¹siedztwa
+        int v;
+        for (int i = 0; i < shortestPath.size() - 1; i++) // dla ka¿dego wêz³a w œcie¿ce powiêkszaj¹cej (bez ostatniego)
+        {
+            v = shortestPath[i];
+            for (int j = 0; j < aList[v].size(); j++) // przeszukaj jego listê s¹siedztwa
+            {
+                if (aList[v][j].to == shortestPath[i + 1]) // ¿eby znaleŸæ ten, który jest nastêpny w œcie¿ce powiêkszaj¹cej
+                {
+                    aList[v][j].remainingFlow -= flows[finish]; // odejmij przep³yw
+                    // zaktualizuj przep³yw w przeciwnym kierunku
+                    for (j = 0; j < aList[shortestPath[i + 1]].size(); j++) // znajdŸ strza³kê w drug¹ stronê
+                    {
+                        if (aList[shortestPath[i + 1]][j].to == shortestPath[i])
+                        {
+                            aList[shortestPath[i + 1]][j].remainingFlow += flows[finish]; // dodaj przep³yw w przeciwnym kierunku
+                        }
+                        break; // droga b->a znaleziona, nie szukaj dalej
+                    }
+                    break; // droga a->b znaleziona, nie szukaj dalej
+                }
+            }
+        }
+    }
+    return totalFlow;
+}
 
 int main()
 {
@@ -63,164 +184,35 @@ int main()
 
 
     ifstream plik("daneZwagami.txt");
-
-    int vertices;
-    int newEdge1, newEdge2, maxFlow;
-    string line;
-
+    //zmienne do wczytania pliku z krawêdziami
+    int vertices, newEdge1, newEdge2, maxFlow;
+    //string line;
+    // wczyta pierwszy wiersz (liczba wêz³ów i krawêdzi)
+    // wczytywanie krawêdzi jest niepotrzebne, ¿eby ³atwiej by³o przekopiowaæ do visualgo
     if (!(plik >> vertices >> newEdge1))
     {
         return -1;
     }
-
-    vector<vector<edgeData>> nList(vertices);
-
-    int lastBrewery; // index of last brewery to multiply max flows of the ones after
+    // tworzy listê s¹siedztwa
+    vector<vector<Road>> aList(vertices);
 
     while (plik >> newEdge1 >> newEdge2 >> maxFlow)
     {
-        // 2. Browary przemna¿aj¹ce przechodz¹c¹ wartoœæ
-        edgeData edgeDataTemp;
-        edgeDataTemp.to = newEdge2;
-        edgeDataTemp.remainingFlow = maxFlow;
-        nList[newEdge1].push_back(edgeDataTemp);
+        // tworzy tymczasow¹ strukturê, uzupe³nia danymi z pliku i dodaje do listy s¹siedztwa
+        Road roadTemp;
+        roadTemp.to = newEdge2;
+        roadTemp.remainingFlow = maxFlow;
+        aList[newEdge1].push_back(roadTemp);
 
+
+        // dodaje do listy s¹siedztwa po³¹czenie w przeciwnym kierunku z zerowym przep³ywem
         // TODO: do this only when path doesn't already exist
-        edgeDataTemp.to = newEdge1;
-        edgeDataTemp.remainingFlow = 0;
-        nList[newEdge2].push_back(edgeDataTemp); // create a path in the opposite direction with no flow
+        roadTemp.to = newEdge1;
+        roadTemp.remainingFlow = 0;
+        aList[newEdge2].push_back(roadTemp);
     }
 
-    int finish = nList.size() - 1;
-    double maxFlowInPath = 0;
-    /// BFS:
-    /// tablica poprzednikow: -2 dla startowego, -1 dla pozostalych, obecny wierzcholek dla dodanych do kolejki
-    /// jesli dostepne 0 przeplywu, nie przechodzimy
-    /// szukamy najkrotszej drogi w sensie ilosci wierzcholkow
-    /// robimy tak dlugo, az nie znajdziemy sciezek powiekszajacych
-    while (true)
-    {
-        /// wyswietl stan listy sasiedztwa
-        cout << "\nLista sasiedztwa: z: ( do, pozostaly ):\n";
-        for (int i = 0; i < vertices - 1; i++)
-        {
-            cout << i << ": \t";
-            for (unsigned int j = 0; j < nList[i].size(); j++)
-            {
-                cout << "( " << nList[i][j].to << ", " << nList[i][j].remainingFlow << " )\t";
-            }
-            cout << endl;
-        }
+    cout << "Max Flow In Whole Path: " << fordFulkerson(aList) << endl;
 
-        int start = 0;
-        vector<bool> visited(nList.size(), false);
-        vector<int> parents(nList.size(), -1);
-        vector<double> distances(nList.size(), INT_MAX);
-        queue<int> Q;
-        parents[start] = -2;
-
-        visited[start] = 1;
-        Q.push(start);
-        int v = 0;
-        while (!Q.empty())
-        {
-            int u = Q.front();
-            Q.pop();
-            //cout<< Q.front()<<" ";
-            for (int i = 0; i < nList[u].size(); i++)
-            {
-                v = nList[u][i].to;
-                if (visited[v] == false && nList[u][i].remainingFlow > 0)
-                {
-                    visited[v] = true;
-                    parents[v] = u;
-                    distances[v] = min(distances[u], nList[u][i].remainingFlow);
-                    Q.push(v);
-                }
-            }
-        }
-
-        cout << "\nparents:\n";
-        for (int i = 0; i < parents.size(); i++)
-        {
-            cout << i << ": " << parents[i] << "\n";
-        }
-
-        cout << "\ndistances:\n";
-        for (int i = 0; i < distances.size(); i++)
-        {
-            cout << i << ": " << distances[i] << "\n";
-        }
-
-        /// nie znaleziono sciezki do ujscia
-        if (parents[finish] == -1) break;
-
-        vector<int> shortestPath;
-        //maxFlowInPath = distances[nList.size() - 1];
-
-        cout << "\nShortest path:\n";
-
-        //cout << finish;
-        shortestPath.push_back(finish);
-        do
-        {
-            finish = parents[finish];
-            //cout << " -> " << finish;
-            shortestPath.push_back(finish);
-        } while (finish != start);
-
-        finish = nList.size() - 1;
-        reverse(shortestPath.begin(), shortestPath.end());
-
-        //cout << endl;
-        for (int i = 0; i < shortestPath.size(); i++)
-        {
-            cout << shortestPath[i] << ", ";
-        }
-        cout << endl;
-
-        cout << "Max Flow In This Path: " << distances[finish] << endl;
-        maxFlowInPath += distances[finish];
-
-        // update flow in the found path
-        for (int i = 0; i < shortestPath.size() - 1; i++)
-        {
-            v = shortestPath[i];
-            // find the edge a->b from shortestPath in adjacency list and subtract flow
-            for (int j = 0; j < nList[v].size(); j++)
-            {
-                if (nList[v][j].to == shortestPath[i + 1])
-                {
-                    if (nList[v][j].nodeType == brewery)
-                    {
-                        nList[v][j].remainingFlow -= distances[finish] * beerConvertsionRate;
-                    }
-                    else
-                    {
-                        nList[v][j].remainingFlow -= distances[finish];
-                    }
-                    // find the opposite direction (b->a) and add flow
-                    for (j = 0; j < nList[shortestPath[i + 1]].size(); j++)
-                    {
-                        if (nList[shortestPath[i + 1]][j].to == shortestPath[i])
-                        {
-                            if (nList[v][j].nodeType == brewery)
-                            {
-                                nList[shortestPath[i + 1]][j].remainingFlow += distances[finish] * beerConvertsionRate;
-                            }
-                            else
-                            {
-                                nList[shortestPath[i + 1]][j].remainingFlow += distances[finish];
-                            }
-                        }
-                        break; // found the edge b->a so stop searching
-                    }
-                    break; // found the edge a->b so stop searching
-                }
-            }
-        }
-    }
-
-    cout << "Max Flow In Whole Path: " << maxFlowInPath << endl;
-
+    return 0;
 }
