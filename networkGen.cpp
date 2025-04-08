ea6711675f1370a1bf5c20c67bd64cbafa296c20
Szlesiowaa <jklonowski@mat.umk.pl>
2025-04-08 14:07:28 +0200

    Add files via upload
    naive network flow input data generation
    
    needs fixing

---
+++ networkGen.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <fstream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    srand(time(nullptr));
+    int vertN, sectionN;
+    int finalConnectionN=0;
+    cin >> vertN;
+
+    sectionN = rand() %3+ (int)log(vertN);
+
+    vector<int> sectionTab(sectionN+2);
+    vector<vector<pair<int, int>>> vertTab(vertN+1);
+
+    int sectionTemp = 0;
+    sectionTab[0]=0;
+    sectionTab[sectionN]=vertN;
+    for (int i = 1; i < sectionN; i++) {
+        sectionTab[i] = rand() % 3 + (vertN / sectionN) + sectionTemp - 2;
+        sectionTemp = sectionTab[i];
+    }
+
+    for(int i=0;i<sectionN;i++)cout << sectionTab[i] << " ";
+    for(int i=1;i<sectionTab[1];i++)vertTab[0].emplace_back(i,rand()%17+5);
+    for(int j=1;j<sectionN;j++)for(int i=sectionTab[j-1];i<sectionTab[j];i++){
+        if(i==0)continue;
+        int connectionN;
+        set<int> uniqueConnections;
+        connectionN=(sectionTab[j+1]-sectionTab[j])/5+rand()%2+1;
+        while(uniqueConnections.size()<connectionN-1)uniqueConnections.insert(rand()%(sectionTab[j+1]-sectionTab[j]+1)+sectionTab[j]);
+        for(auto it=uniqueConnections.begin();it!=uniqueConnections.end();it++)vertTab[i].emplace_back(*it,rand()%17+5);
+    }
+    for(int i=sectionTab[sectionN-1];i<vertN+1;i++)vertTab[i].emplace_back(vertN+1,rand()%17+5);
+    for(int i=0;i<vertN+1;i++)finalConnectionN+=vertTab[i].size();
+    ofstream file("daneZwagami.txt");
+    file<<vertN+2<<" "<<finalConnectionN;
+    for(int i=0;i<vertN+1;i++)for(int j=0;j<vertTab[i].size();j++)file<<endl<<i<<" "<<vertTab[i][j].first<<" "<<vertTab[i][j].second;
+
+    cout<<endl<<vertN+2<<" "<<finalConnectionN<<endl;
+    for(int i=0;i<vertN+1;i++)for(int j=0;j<vertTab[i].size();j++)cout<<endl<<i<<" "<<vertTab[i][j].first<<" "<<vertTab[i][j].second;
+
+    file.close();
+    return 0;
+}
